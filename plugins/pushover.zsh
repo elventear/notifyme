52e176527bd1ad1dee2c34bbccc7f6a1bea9a6ce
Pepe Barbe <pbarbe@bloomhealthco.com>
2014-01-21 09:35:48 -0600

    Do not pollute enviroment with pushover API keys. Explain how it works.

--- plugins/pushover.zsh
+++ plugins/pushover.zsh
@@ -1,6 +1,16 @@
 # Emits http calls to the  https://pushover.net service
 
+# Requires the API keys for Pushover in ~/.PUSHOVER_TOKEN exposed as
+# PUSHOVER_APP_TOKEN and PUSHOVER_USER_KEY
+#
+# Example:
+#
+# PUSHOVER_USER_KEY=keyval
+# PUSHOVER_APP_TOKEN=keyval
+
 function notifyme-pushover() {
+    (local -r TOKEN=~/.PUSHOVER_TOKEN
+    source $TOKEN > /dev/null
     local MESSAGE="$1 $2"
     if [ -n "$PUSHOVER_APP_TOKEN" -a -n "$PUSHOVER_USER_KEY" ]; then
         test ${#MESSAGE} -gt 0 || return 1
@@ -9,10 +19,10 @@ function notifyme-pushover() {
             -F "user=$PUSHOVER_USER_KEY" \
             -F "message=$MESSAGE" \
             https://api.pushover.net/1/messages.json > /dev/null
-    fi
+    fi)
 }
 
 function {
-    local TOKEN=~/.PUSHOVER_TOKEN
-    _notifyme-exists curl && [[ -f $TOKEN ]] && source $TOKEN > /dev/null ||  unset -f notifyme-pushover
+    local -r TOKEN=~/.PUSHOVER_TOKEN
+    _notifyme-exists curl && [[ -f $TOKEN ]] ||  unset -f notifyme-pushover
 }
